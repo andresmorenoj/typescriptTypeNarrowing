0ae9b6cadd23f2c9c4eacd629e4084900b8e32ee
Andres Moreno <andresmorenojf@gmail.com>
2024-01-21 01:12:28 -0500

    Instance of Narrowing

--- src/index.ts
+++ src/index.ts
@@ -19,28 +19,52 @@ const printChars = (word?: string) => {
 // EQUALITY NARROWING
 function someDemo(x: string | number, y: string | boolean) {
   if (x === y) {
-		x.toUpperCase()
+    x.toUpperCase();
   }
-};
+}
 
-someDemo(3, '3');
+someDemo(3, "3");
 /* ****************** */
 
 interface IMovie {
-	title: string;
-	duration: number;
-};
+  title: string;
+  duration: number;
+}
 
 interface ITVShow {
-	title: string;
-	numEpisodes: number;
-	episodeDuration: number;
-};
+  title: string;
+  numEpisodes: number;
+  episodeDuration: number;
+}
 
 function getMedia(media: IMovie | ITVShow) {
-	if('numEpisodes' in media) {
-		return media.numEpisodes * media.episodeDuration;
-	}
+  if ("numEpisodes" in media) {
+    return media.numEpisodes * media.episodeDuration;
+  }
 
-	return media.duration;
+  return media.duration;
+}
+
+function printFullDate(date: string | Date) {
+  if (date instanceof Date) {
+    console.log(date.toUTCString());
+  } else {
+    console.log(new Date(date).toUTCString());
+  }
+}
+
+class User {
+  constructor(public username: string) {}
+}
+
+class Company {
+  constructor(public name: string) {}
+}
+
+function printName(entity: User | Company) {
+  if (entity instanceof User) {
+    console.log(entity.username);
+  } else {
+    console.log(entity.name);
+  }
 }
